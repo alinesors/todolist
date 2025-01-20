09f378d65286f1c340c0401f2691445a3b2a527e
Aline Soares <aliinanda@example.com>
2025-01-20 11:23:56 -0300

    add

---
+++ src/test/java/br/com/alinesors/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alinesors.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
